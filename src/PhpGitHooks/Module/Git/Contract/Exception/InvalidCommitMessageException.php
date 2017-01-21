329c90067c4f41329a1054a81850da2aae107fc8
JasonQSY <syqian@sjtu.edu.cn>
2017-01-21 15:40:22 +0800

    change the message of commit-msg error

--- src/PhpGitHooks/Module/Git/Contract/Exception/InvalidCommitMessageException.php
+++ src/PhpGitHooks/Module/Git/Contract/Exception/InvalidCommitMessageException.php
@@ -4,5 +4,5 @@ namespace PhpGitHooks\Module\Git\Contract\Exception;
 
 class InvalidCommitMessageException extends \Exception
 {
-    protected $message = 'Invalid Commit message: commit message for does not contain issue #<number> reference!';
+    protected $message = 'Invalid Commit message: commit message not matching the requested style of the project.';
 }
